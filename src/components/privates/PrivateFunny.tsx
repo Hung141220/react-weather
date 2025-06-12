bfd4e5db1451618753da93e829617cfe6c5e3e9b
lh <hunglm@gmail.com>
2025-06-12 13:38:40 +0700

    a

--- src/components/privates/PrivateFunny.tsx
+++ src/components/privates/PrivateFunny.tsx
@@ -1,13 +1,14 @@
+import { Outlet } from "react-router-dom";
 import PasswordPrompt from "./PasswordPrompt";
 import { useSelector } from "react-redux";
 
-const PrivateFunny = ({ children }: { children: React.ReactNode }) => {
+const PrivateFunny = () => {
   const { isAuthenticated } = useSelector<
     { funny: { isAuthenticated: boolean; error: string } },
     { isAuthenticated: boolean; error: string }
   >((state) => state.funny);
 
-  return isAuthenticated ? children : <PasswordPrompt />;
+  return isAuthenticated ? <Outlet /> : <PasswordPrompt />;
 };
 
 export default PrivateFunny;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,29 +17,31 @@ export const router = createBrowserRouter([
     path: "weather",
     element: <Weather />,
   },
-  {
-    path: "darling",
-    element: <DarlingPage />,
-  },
+
   {
     path: "todolist",
     element: <TodolistPage />,
   },
   {
-    path: "funny",
-    element: (
-      <PrivateFunny>
-        <FunnyPage />
-      </PrivateFunny>
-    ),
-  },
-  {
-    path: "info-love",
-    element: <InfoLoveLayout />,
+    element: <PrivateFunny />,
     children: [
       {
-        index: true,
-        element: <HomePage />,
+        path: "darling",
+        element: <DarlingPage />,
+      },
+      {
+        path: "funny",
+        element: <FunnyPage />,
+      },
+      {
+        path: "info-love",
+        element: <InfoLoveLayout />,
+        children: [
+          {
+            index: true,
+            element: <HomePage />,
+          },
+        ],
       },
     ],
   },
@@ -47,43 +49,4 @@ export const router = createBrowserRouter([
     path: "*",
     element: <Navigate to="/" replace />,
   },
-
-  //   {
-  //     // path: '/',
-  //     // element: <Layout />,
-  //     // errorElement: <NotFoundPage />, // Component sẽ hiển thị nếu có lỗi xảy ra trong route này hoặc các route con của nó
-  //     // children: [
-  //     //   {
-  //     //     index: true, // Đây là route mặc định cho path cha ('/')
-  //     //     element: <HomePage />,
-  //     //   },
-  //     //   {
-  //     //     path: 'about', // Path tương đối với path cha ('/') => /about
-  //     //     element: <AboutPage />,
-  //     //   },
-  //     //   {
-  //     //     path: 'contact', // Path tương đối với path cha ('/') => /contact
-  //     //     element: <ContactPage />,
-  //     //   },
-  //     //   // Thêm các route con khác vào đây
-  //     //   // Ví dụ:
-  //     //   // {
-  //     //   //   path: 'products',
-  //     //   //   element: <ProductsPage />,
-  //     //   // },
-  //     //   // {
-  //     //   //   path: 'products/:id', // Route động
-  //     //   //   element: <ProductDetailPage />,
-  //     //   // },
-  //     // ],
-  //   },
-  // Các route cấp cao khác không cần Layout chung có thể được định nghĩa ở đây
-  // {
-  //   path: '/login',
-  //   element: <LoginPage />,
-  // },
-  //   {
-  //     path: '*', // Ký tự đại diện cho bất kỳ đường dẫn nào không khớp với các route trên
-  //     element: <NotFoundPage />, // Component hiển thị trang 404 Not Found
-  //   },
 ]);
